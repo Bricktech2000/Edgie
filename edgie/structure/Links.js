ba32d5cafb6566010e3745915b38aa3932dd2f99
Bricktech <Bricktech2000@gmail.com>
2021-09-12 23:29:07 -0400

    changed button alignment to center of page

--- edgie/structure/Links.js
+++ edgie/structure/Links.js
@@ -6,41 +6,43 @@ import styles from './Links.module.css';
 const Links = () => {
   return (
     <main className={styles.Links}>
-      <Link
-        icon="fas fa-envelope"
-        href="mailto:bricktech2000@gmail.com"
-        color="#bb001b"
-      >
-        Mail
-      </Link>
-      <Link
-        icon="fab fa-github"
-        href="https://github.com/Bricktech2000/"
-        color="#222"
-      >
-        Github
-      </Link>
-      <Link
-        icon="fab fa-facebook-f"
-        href="https://github.com/Bricktech2000/"
-        color="#4267B2"
-      >
-        Facebook
-      </Link>
-      <Link
-        icon="fab fa-discord"
-        href="https://discord.gg/vU9DA6jUz8"
-        color="#7289da"
-      >
-        Discord
-      </Link>
-      <Link
-        icon="fab fa-snapchat-ghost"
-        href="https://www.snapchat.com/add/emilien_breton?share_id=09KhRyzndOk"
-        color="#DDDA00"
-      >
-        Snapchat
-      </Link>
+      <div className={styles.container}>
+        <Link
+          icon="fas fa-envelope"
+          href="mailto:bricktech2000@gmail.com"
+          color="#bb001b"
+        >
+          Mail
+        </Link>
+        <Link
+          icon="fab fa-github"
+          href="https://github.com/Bricktech2000/"
+          color="#222"
+        >
+          Github
+        </Link>
+        <Link
+          icon="fab fa-facebook-f"
+          href="https://github.com/Bricktech2000/"
+          color="#4267B2"
+        >
+          Facebook
+        </Link>
+        <Link
+          icon="fab fa-discord"
+          href="https://discord.gg/vU9DA6jUz8"
+          color="#7289da"
+        >
+          Discord
+        </Link>
+        <Link
+          icon="fab fa-snapchat-ghost"
+          href="https://www.snapchat.com/add/emilien_breton?share_id=09KhRyzndOk"
+          color="#DDDA00"
+        >
+          Snapchat
+        </Link>
+      </div>
     </main>
   );
 };
